ea5d2b7eef9a70dd5aa982c3c7e99c4f58129b87
Ricardo Ferreira da Silva <tansvaino@hotmail.com>
2019-04-16 13:49:29 +0100

    Correctly calling the predicates.

--- jsAst/src/javascript/parseJavascript.js
+++ jsAst/src/javascript/parseJavascript.js
@@ -1,6 +1,2 @@
-//load(esprimaPath);
-print("START AST PARSING");
 var ast = esprima.parseScript(code,{loc:true,comment:true});
-print("AFTER AST PARSING");
 var string = JSON.stringify(ast);
-print("AFTER STRINGIFY");
